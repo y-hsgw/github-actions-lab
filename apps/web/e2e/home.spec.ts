41054b9ea8d78fb452f5f07fa867c383ae0048c8
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-03-04 21:04:08 +0900

    api繋ぎ込み箇所のe2e検証

--- apps/web/e2e/home.spec.ts
+++ apps/web/e2e/home.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
 
   await expect(page).toHaveTitle("Hello, Next.js!");
+});
+
+test("has cats", async ({ page }) => {
+  await page.goto("/");
+
+  const listitem = page.getByRole("list").getByRole("listitem");
+  await expect(listitem.getByText("name: Kitty")).toBeVisible();
+  await expect(listitem.getByText("age: 1")).toBeVisible();
 });
