93e0eb8ce54bdd1964c19f62d68e2e1df407e190
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-03-04 20:45:25 +0900

    catsのAPI繋ぎ込み

--- apps/api/src/cats/cats.service.ts
+++ apps/api/src/cats/cats.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { Cat } from './interfaces/cat.interface.js';
+import { Injectable } from "@nestjs/common";
+import { Cat } from "./interfaces/cat.interface.js";
 
 @Injectable()
 export class CatsService {
-  private readonly cats: Cat[] = [];
+  private readonly cats: Cat[] = [
+    {
+      name: "Kitty",
+      age: 1,
+      breed: "British Shorthair",
+    },
+  ];
 
   create(cat: Cat) {
     this.cats.push(cat);
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,6 +4,28 @@ export const metadata: Metadata = {
   title: "Hello, Next.js!",
 };
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+interface Cat {
+  age: number;
+  name: string;
+  bread: string;
+}
+
+export default async function Page() {
+  const data = await fetch("http://localhost:8080/cats");
+  const cats: Cat[] = await data.json();
+
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <h2>Cats List</h2>
+      <ul>
+        {cats.map((cat) => (
+          <li key={cat.name}>
+            <p>name: {cat.name}</p>
+            <p>age: {cat.age}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
