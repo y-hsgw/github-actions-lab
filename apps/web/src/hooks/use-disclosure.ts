9b157d82216f0ac53f61c30baa280f0effdb6a49
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-03-12 23:17:27 +0900

    useDisclosureの作成

---
+++ apps/web/src/hooks/use-disclosure.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const useDisclosure = (initialIsOpen = false) => {
+  const [isOpen, setIsOpen] = useState(initialIsOpen);
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  return { isOpen, open, close };
+};
