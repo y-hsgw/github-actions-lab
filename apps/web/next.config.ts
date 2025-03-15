367cdfb516e4d61c9d73df443a0602bee89f8ed3
Yukihiro Hasegawa <49516827+y-hsgw@users.noreply.github.com>
2025-03-15 11:20:37 +0900

    WebCIの更新 (#21)
    * lintとbuildにジョブを変更
    
    * nextConfigからignoreBuildErrorsを解除
    
    * ジョブ名をユニークに変更

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   experimental: {
     typedRoutes: true,
   },
